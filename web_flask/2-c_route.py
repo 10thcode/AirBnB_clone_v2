c155c452e4a68123cede201a0a5abddde2a39fac
10thcode <attabadukwabena@gmail.com>
2023-10-23 00:41:34 +0000

    Add 2-c_route.py
    a script that starts a Flask web application

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+'''
+A script that starts a Flask web application.
+'''
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home_page():
+    '''
+    Displays a text
+    '''
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    '''
+    Displays a text
+    '''
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    '''
+    Displays a text
+
+    Args:
+        text (string): the text to be displayed.
+    '''
+    return "C {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
